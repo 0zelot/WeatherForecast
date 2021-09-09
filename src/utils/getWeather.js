08adee00f4d1d433daedfc3feff5276e9a3bc746
unknown <ozelot@ctemplar.com>
2021-09-09 20:20:20 +0200

    Optimized getWeather function

--- src/utils/getWeather.js
+++ src/utils/getWeather.js
@@ -1,21 +1,14 @@
 import convert from "xml-js";
 
 const getWeather = async (query) => {
-    let result;
-    await fetch(`https://weather.service.msn.com/find.aspx?src=outlook&weadegreetype=C&culture=en_US&weasearchstr=${query.toString()}`).then(async response => response.text()).then(async body => {
-        if(body) {
-            try {
-                result = convert.xml2json(body, {compact: true, spaces: 2});
-            } catch(err) {
-                result = '{"error":"Failed to convert data."}';
-            }
-        } else {
-            result = '{"error":"Body is empty."}';
-        }
-    }).catch(err => {
-        result = '{"error":"Failed to fetch."}';
-    });
-    return result;
+    const data = await fetch(`https://weather.service.msn.com/find.aspx?src=outlook&weadegreetype=C&culture=en_US&weasearchstr=${query.toString()}`);
+    const body = await data.text();
+    if(!body) return {error: "Body is empty."};
+    try {
+        return convert.xml2json(body, {compact: true, spaces: 2});
+    } catch(err) {
+        return {error: "Failed to convert data."};
+    }
 }
 
 export default getWeather;
