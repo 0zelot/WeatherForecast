47f786169e82e3579a23627f36a93ba3ec8cd05d
unknown <ozelot@ctemplar.com>
2021-09-09 20:25:21 +0200

    Optimized getLanguage function

--- src/utils/getLanguage.js
+++ src/utils/getLanguage.js
@@ -3,30 +3,21 @@ import language from "../config.json";
 const lang = (msg) => {
     let message;
     const detected = navigator.languages;
-    if(detected) {
-        let l = detected[0];
-        if(l.includes("-")) {
-            l = l.split("-");
-            l = l[0];
-        }
+    if(!detected) return;
+    let l = detected[0];
+    if(l.includes("-")) l = l.split("-")[0];
+    
+    if(language.translates[l]) {
+        message = language.translates[l][msg];
+        if(!msg) message = l;
+
+        l = detected[1];
         if(language.translates[l]) {
             message = language.translates[l][msg];
-            if(!msg) {
-                message = l;
-            }
-        } else {
-            l = detected[1];
-            if(language.translates[l]) {
-                message = language.translates[l][msg];
-                if(!msg) {
-                    message = l;
-                }
-            } else {
-                message = language.translates.en[msg];
-                if(!msg) {
-                    message = "default";
-                }
-            }
+            if(!msg) message = l;
+
+            message = language.translates.en[msg];
+            if(!msg) message = "default";
         }
     }
     return message;
